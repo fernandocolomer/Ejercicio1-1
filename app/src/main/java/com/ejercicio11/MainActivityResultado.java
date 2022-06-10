f9f683d403cc3fc4571540ecb32198e5c57c5fdf
fernandocolomer <fernandocolomer@icloud.com>
2022-06-09 18:44:34 -0600

    Initial commit

---
+++ app/src/main/java/com/ejercicio11/MainActivityResultado.java
@@ -0,0 +1,13 @@
+package com.ejercicio11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityResultado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_resultado);
+    }
+}
