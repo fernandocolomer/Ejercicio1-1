4a322725564bf98d8f10c9f7886d83939a16a0e6
ZandyCortez <zandycortez2000@gmail.com>
2022-06-14 10:20:44 -0600

    Actualizacion en los activitis , de resta,suma,multiplicacion ,division

--- app/src/main/java/com/ejercicio11/MainActivity.java
+++ app/src/main/java/com/ejercicio11/MainActivity.java
@@ -1,13 +1,81 @@
 package com.ejercicio11;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextN1, editTextN2;
+    Button botonSumar, botonRestar,botonMultiplicar, botonDividir;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // inicializamos las variables
+        editTextN1 = (EditText) findViewById(R.id.txtnumerouno);
+        editTextN2 = (EditText) findViewById(R.id.txtnumerodos);
+
+        botonSumar = (Button) findViewById(R.id.btnsuma);
+        botonRestar = (Button) findViewById(R.id.btnresta);
+        botonMultiplicar = (Button) findViewById(R.id.btnmultiplicacion);
+        botonDividir = (Button) findViewById(R.id.btndivision);
+
+        botonSumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double numero1,numero2,resultado;
+                numero1=Double.valueOf(editTextN1.getText().toString());
+                numero2=Double.valueOf(editTextN2.getText().toString());
+                resultado=numero1+numero2;
+                Intent intent = new Intent(getApplicationContext(),MainActivityResultado.class);
+                intent.putExtra("resultado","Total de la suma: "+resultado);
+
+            }
+        });
+
+        botonRestar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double numero1,numero2,resultado;
+                numero1=Double.valueOf(editTextN1.getText().toString());
+                numero2=Double.valueOf(editTextN2.getText().toString());
+                resultado=numero1-numero2;
+                Intent intent = new Intent(getApplicationContext(),MainActivityResultado.class);
+                intent.putExtra("resultado","Total de la resta: "+resultado);
+
+            }
+        });
+
+        botonDividir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double numero1,numero2,resultado;
+                numero1=Double.valueOf(editTextN1.getText().toString());
+                numero2=Double.valueOf(editTextN2.getText().toString());
+                resultado=numero1/numero2;
+                Intent intent = new Intent(getApplicationContext(),MainActivityResultado.class);
+                intent.putExtra("resultado","Total de la division: "+resultado);
+            }
+        });
+
+        botonMultiplicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double numero1,numero2,resultado;
+                numero1=Double.valueOf(editTextN1.getText().toString());
+                numero2=Double.valueOf(editTextN2.getText().toString());
+                resultado=numero1*numero2;
+                Intent intent = new Intent(getApplicationContext(),MainActivityResultado.class);
+                intent.putExtra("resultado","Total de la multiplicacion: "+resultado);
+            }
+
+        });
     }
 }
--- app/src/main/java/com/ejercicio11/MainActivityResultado.java
+++ app/src/main/java/com/ejercicio11/MainActivityResultado.java
@@ -2,6 +2,7 @@ package com.ejercicio11;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivityResultado extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivityResultado extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_resultado);
+
+
+        TextView resultado = (TextView) findViewById(R.id.textViewResultado);
+        resultado.setText("");
+        resultado.setText(getIntent().getStringExtra("resultado").toString());
+
     }
 }
